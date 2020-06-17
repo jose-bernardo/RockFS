66dd534bc122b4f08f073c66aeee8b4d521b6883
David Matos <david.matos21@gmail.com>
2020-06-17 11:08:14 +0100

    Fixed a bug with the installation process

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ echo "###############################################"
 #echo "Installing PVSS..."
 #mvn install:install-file -Dfile=lib/pvss.jar -DgroupId=com.ufsc -DartifactId=pvss -Dversion=1.0-SNAPSHOT -Dpackaging=jar
 
+DIR=`pwd`
 
 echo "Installing JReedSolEC..."
 mvn install:install-file -Dfile=lib/JavaReedSolomon.jar -DgroupId=backblaze.backblaze -DartifactId=JavaReedSolomon -Dversion=1.0-SNAPSHOT -Dpackaging=jar
@@ -16,10 +17,10 @@ cd DepSpacito
 echo "Installing Depspacito"
 sh build_args.sh 127.0.0.1 8000
 mvn install
-cd..
+cd $DIR
 
 echo "Installing SafeCloudFS..."
-mvn compile
+mvn -s settings.xml compile
 
 echo "Finished installing"
 
